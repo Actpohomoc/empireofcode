551ef81c42291f90566ef782245c9d5056830c50
Actpohomoc <Actpohomoc@users.noreply.github.com>
2016-06-10 14:26:13 +0300

    Update vault_password.py

--- vault_password.py
+++ vault_password.py
@@ -1,1 +1,1 @@
-def golf(p): return len(p)>9 and p!=p.lower() and p!=p.upper() and any('0'<=l and l<='9' for l in p)
+def golf(p): return len(p)>9 and p!=p.lower() and p!=p.upper() and any('0'<=l<='9' for l in p)
